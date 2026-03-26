125a3b471f15e7e6c4827a757cbd10ea738a2cfd
ammar869 <ammarhassan1131318@gmail.com>
2026-03-27 01:22:54 +0500

    seting  up the api

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// const apiKey = '78440dc2c43cba6e2ca0e18ffddca0c4';
-// const audioUrl = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
+const apiKey = '78440dc2c43cba6e2ca0e18ffddca0c4';
+const audioUrl = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
 
-// async function recognizeSong() {
-//   try {
-//     const response = await axios.post('https://api.audd.io/', null, {
-//       params: { api_token: apiKey, url: audioUrl }
-//     });
+async function recognizeSong() {
+  try {
+    const response = await axios.post('https://api.audd.io/', null, {
+      params: { api_token: apiKey, url: audioUrl }
+    });
 
-//     console.log('Song info:', response.data);
-//   } catch (error) {
-//     console.error('Error:', error.message);
-//   }
-// }
+    console.log('Song info:', response.data);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
 
-// recognizeSong();
+recognizeSong();
+
 console.log("Starting script...");
 
 async function recognizeSong() {
